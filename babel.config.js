788336fa84bd98fd2fac422181852c7b07c649f3
TsaoneTimothy <tsaonetimothy@gmail.com>
2025-05-07 16:19:07 +0200

    fix: Update Babel configuration for class properties and private methods

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,12 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      '@babel/plugin-transform-nullish-coalescing-operator',
       '@babel/plugin-transform-class-properties',
+      '@babel/plugin-transform-private-methods',
+      '@babel/plugin-transform-private-property-in-object',
+      '@babel/plugin-transform-nullish-coalescing-operator',
       '@babel/plugin-transform-optional-chaining',
-      'react-native-reanimated/plugin',
+      'expo-router/babel',
     ],
   };
 }; 
